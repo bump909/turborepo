2d096180cba39c5f1bc7649bbe56912afe274858
Kris Kristiansen <bump909@hotmail.com>
2024-03-15 07:51:49 +0000

    add sampleshed link to top menu

--- apps/web/src/app/components/app-bar.tsx
+++ apps/web/src/app/components/app-bar.tsx
@@ -7,11 +7,14 @@ import ToastExample from './toast-example'
 async function AppBar() {
   const session = await auth()
   return (
-    <div className="flex min-h-12 w-full justify-between gap-2 bg-gradient-to-b from-slate-800 to-slate-600 p-2 text-slate-50">
+    <div className="flex min-h-12 w-full justify-between gap-2 bg-gradient-to-b from-slate-300 to-slate-200 dark:from-slate-800 dark:to-slate-600 p-2 dark:text-slate-50 text-slate-900">
       <div className="mx-auto flex w-full max-w-7xl items-center gap-x-4 px-4">
         {/* <Link href={'/clientPage'}>Client Page</Link>
         <Link href={'/serverPage'}>Server Page</Link>
         <Link href={'/middlewareProtected'}>Middleware Protected Page</Link> */}
+        <Link href="/" className=" font-semibold text-2xl">
+          THE SAMPLE SHED
+        </Link>
         <div className="ml-auto flex items-center gap-x-2">
           <ModeToggle />
           <ToastExample />
